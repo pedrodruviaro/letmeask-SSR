091ba14e5441a057fe747240ae13436ae8ca9d21
pedrodruviaro <pedrodruviaro@gmail.com>
2021-10-21 16:49:49 -0300

    organizando projeto

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use("/", (req, res) => res.render("home"));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
